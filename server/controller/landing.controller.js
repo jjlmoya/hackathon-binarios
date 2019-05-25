44e15bb5fc43fe1d719aeac6e236376a2d54395f
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-05-25 20:11:37 +0200

    Haiku#14 Arreglar Controladores !Cuántos dolores¡

--- server/controller/landing.controller.js
+++ server/controller/landing.controller.js
@@ -1,68 +1,37 @@
 const router = require('../config').router();
 const Commons = require('../services/commons.services');
 
-const mongoose = require('mongoose');
-const ConfigDBModels = require('../config-db-models');
-
-let models = ConfigDBModels.createSchemas(mongoose);
-
 
 module.exports = function (app) {
-// getFilm('/pelicula/la-comunidad-del-anillo');
+    router.forEach(function (corePage) {
+        app.get(corePage.path, function (req, res) {
+            if (corePage.service) {
+                corePage.service().then((result) => {
+                    let model = Object.assign({}, corePage, {model: result});
+                    console.log('object %o', model);
+                    res.render(corePage.view, Commons.join(model));
+                });
+            } else {
+                res.render(corePage.view, Commons.join(corePage));
+            }
+        });
+        if (corePage.pages) {
+            corePage.pages.forEach(function (page) {
+                app.get(corePage.path + page.path, function (req, res) {
+                    if (page.service) {
+                        page.service().then((result) => {
+                            let model = Object.assign({}, corePage, {model: result});
+                            console.log('object %o', model);
+                            res.render(page.view, Commons.join(model));
+                        });
+                    } else {
+                        res.render(page.view, Commons.join(corePage));
+                    }
 
-  // app.get('/', function (req, res) {
-  //   console.log(Commons.join(corePage));
-  //     res.render(corePage.view, Commons.join(corePage));
-  // });
-
-
-
-  app.get('/hackathon/personajes', function (req, res) {
-    let characters = getCharacters().then((result) => {
-      console.log(result);
-
-      res.render('pages/hackathon/characters',
-        Commons.join(Commons.join({characters: result})));
+                });
+            });
+        }
     });
 
-  });
-
-
-
-  // app.get('hackaton/personaje/{character}', function (req, res) {
-  //   //getModelPersonaje(character)
-  //     res.render(corePage.view, Commons.join(corePage));
-  // });
 };
 
-getCharacters = function () {
-    var query = models.Character.find();
-
-   return query.exec();
-}
-
-getCharacter = function (characterUrl) {
-    var query = models.Character.findOne({
-      'pageUrl': characterUrl,
-    });
-
-   return query.exec();
-}
-
-getFilms = function () {
-    var query = models.Film
-    .find()
-    .populate('characters');
-
-    return query.exec();
-}
-
-getFilm = function (filmUrl) {
-    var query = models.Film
-    .findOne({
-      'pageUrl': filmUrl,
-    })
-    .populate('characters');
-
-    return query.exec();
-}
--- server/pages/home.js
+++ server/pages/home.js
@@ -1,124 +1,61 @@
+const mongoose = require('mongoose');
+const ConfigDBModels = require('../config-db-models');
+
+let models = ConfigDBModels.createSchemas(mongoose);
+
+
+let getCharacters = function () {
+        var query = models.Character.find();
+        return query.exec();
+    },
+
+    getCharacter = function (characterUrl) {
+        var query = models.Character.findOne({
+            'pageUrl': characterUrl,
+        });
+
+        return query.exec();
+    },
+
+    getFilms = function () {
+        var query = models.Film
+            .find()
+            .populate('c');
+
+        return query.exec();
+    },
+
+    getFilm = function (filmUrl) {
+        var query = models.Film
+            .findOne({
+                'pageUrl': filmUrl,
+            })
+            .populate('characters');
+
+        return query.exec();
+    };
+
 module.exports = {
     path: '/hackathon/',
     view: 'pages/hackathon/index',
     layout: 'main',
     brand: 'purple-skies',
     name: 'Spoiler Free',
+    service: getFilms,
     logo: 'https://i.pinimg.com/originals/f6/c6/08/f6c6083e49a284ec558ef7380391006f.png',
-    model: {
-        character: {
-            name: 'Gollum',
-            alive: true,
-            ring: true,
-            image: 'https://www.syfy.com/sites/syfy/files/styles/1200x680/public/wire/legacy/images/GollumwithFish.jpg',
-            race: 'Hobbit',
-            others: [
-                {
-                    link: '',
-                    name: 'Samsagaz Gamyi',
-                    image: 'http://images2.fanpop.com/images/photos/3000000/Sam-lord-of-the-rings-3067944-1024-768.jpg',
-                },
-                {
-                    link: '',
-                    name: 'Frodo Bolsón',
-                    image: 'https://pbs.twimg.com/profile_images/914686805991190528/IgdbIKmV_400x400.jpg',
-                },
-                {
-                    link: '',
-                    name: 'Bilbo Bolsón',
-                    image: 'https://i.ytimg.com/vi/jqZInPXXHsQ/maxresdefault.jpg',
-                }]
-
-        },
-        characters: [
-            {
-                link: '',
-                name: 'Frodo Bolsón',
-                image: 'https://pbs.twimg.com/profile_images/914686805991190528/IgdbIKmV_400x400.jpg',
-            },
-            {
-                link: '',
-                name: 'Boromir',
-                image: 'https://vignette.wikia.nocookie.net/lotr/images/b/b4/Seanbean_boromir.jpg/revision/latest?cb=20110327195115',
-            },
-            {
-                link: '',
-                name: 'Gandalf',
-                image: 'https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-the-lord-of-the-rings-ian-mckellen.jpg'
-            },
-            {
-                link: '',
-                name: 'Saruman',
-                image: 'https://vignette.wikia.nocookie.net/lotr/images/0/0c/Christopher_Lee_as_Saruman.jpg/revision/latest?cb=20170127113833'
-            },
-            {
-                link: '',
-                name: 'Galadriel',
-                image: 'https://vignette.wikia.nocookie.net/lotr/images/0/0c/Christopher_Lee_as_Saruman.jpg/revision/latest?cb=20170127113833'
-            },
-            {
-                link: '',
-                name: 'Abc',
-                image: 'https://vignette.wikia.nocookie.net/lotr/images/0/0c/Christopher_Lee_as_Saruman.jpg/revision/latest?cb=20170127113833'
-            },
-            {
-                link: '',
-                name: 'Saruman',
-                image: 'https://vignette.wikia.nocookie.net/lotr/images/0/0c/Christopher_Lee_as_Saruman.jpg/revision/latest?cb=20170127113833'
-            }
-
-        ],
-        films: [
-            {
-                name: 'La Comunidad del Anillo',
-                image: 'http://elanillounico.com/wp-content/uploads/2016/12/ESDLA.-LCDA.jpg',
-                link: '',
-                characters: [
-                    {
-                        link: '',
-                        name: 'Frodo Bolsón',
-                        image: 'https://pbs.twimg.com/profile_images/914686805991190528/IgdbIKmV_400x400.jpg',
-                    }
-                ]
-            },
-            {
-                name: 'Las Dos Torres',
-                link: '',
-                image: 'https://i0.wp.com/elanillounico.com/wp-content/uploads/2015/11/ESDLA.-LDT1.jpg',
-                characters: [
-                    {
-                        link: '',
-                        name: 'Frodo Bolsón',
-                        image: 'https://pbs.twimg.com/profile_images/914686805991190528/IgdbIKmV_400x400.jpg',
-                    }
-                ]
-            },
-            {
-                name: 'El Retorno del Rey',
-                link: '',
-                image: 'https://i.pinimg.com/originals/a1/12/27/a11227c7c318087a61e6cabfc9ef67bb.jpg',
-                characters: [
-                    {
-                        link: '',
-                        name: 'Frodo Bolsón',
-                        image: 'https://pbs.twimg.com/profile_images/914686805991190528/IgdbIKmV_400x400.jpg',
-                    }
-                ]
-            }
-        ]
-    },
     pages: [
         {
             path: 'personajes',
             view: 'pages/hackathon/characters',
             layout: 'main',
-            name: 'Personajes'
+            name: 'Personajes',
+            service: getCharacters
         },
         {
             path: 'personaje/gollum',
             view: 'pages/hackathon/gollum',
             layout: 'main',
-            name: 'Gollum'
+            name: 'Gollum',
         }
     ],
 };
