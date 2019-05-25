66a91603c36e84c03585938296c3d9f1ae856998
W01fw00d <gabrm_91@hotmail.com>
2019-05-25 17:52:25 +0200

    HAIKU#8
    Introducir contenido en DB, dropdatabase en cada ejecución para evitar duplicar
    
    Y todo este curro, sin siquiera esperar cobrar

--- server/init-db.js
+++ server/init-db.js
@@ -11,7 +11,7 @@ mongoose.connect(Config.db().protocol + Config.db().url + Config.db().dbName,
 
     createSchemas();
 
-    saveLaComarca();
+    dropDatabase()
 })
 
 createSchemas = function () {
@@ -19,6 +19,7 @@ createSchemas = function () {
       name: String,
       extension: Number,
       pageUrl: String,
+      theme: String,
   });
 
   var characterSchema = new mongoose.Schema({
@@ -28,8 +29,10 @@ createSchemas = function () {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Place',
       },
+      alive: Boolean,
       imageUrl: String,
       pageUrl: String,
+      theme: String,
   });
 
   var filmSchema = new mongoose.Schema({
@@ -47,6 +50,7 @@ createSchemas = function () {
       }],
       imageUrl: String,
       pageUrl: String,
+      theme: String,
     });
 
   Place = mongoose.model('Place', placeSchema);
@@ -54,47 +58,127 @@ createSchemas = function () {
   Film = mongoose.model('Film', filmSchema);
 }
 
-saveLaComarca = function () {
-  let laComarcaPlace = new Place({
+dropDatabase = function () {
+  Place.remove({}, function(err) {
+   console.log('Places removed');
+
+   Character.remove({}, function(err) {
+    console.log('Characters removed');
+
+    Film.remove({}, function(err) {
+     console.log('Films removed');
+
+     save1();
+    });
+   });
+  });
+}
+
+save1 = function () {
+  let doc = new Place({
     name: 'La Comarca',
     extension: 100,
     pageUrl: '/zona/la-comarca',
+    theme: 'aviator',
   });
 
-  laComarcaPlace.save((error, result) => saveBilbo(error, result));
+  doc.save((error, result) => save2(error, result));
 }
 
-saveBilbo = function (error, laComarca) {
+save2 = function (error, laComarca) {
   if (error) return console.error(error);
-  var bilboCharacter = new Character({
-    name: 'Bilbo',
+  var doc = new Character({
+    name: 'Frodo Bolsón',
     demographicData: ['Hobbit', 'Bolson', 'PortadorAnillo',],
     placeOfBirth: laComarca.id,
-    imageUrl: 'bilbo',
-    pageUrl: '/personaje/bilbo',
+    alive: true,
+    imageUrl: 'https://pbs.twimg.com/profile_images/914686805991190528/IgdbIKmV_400x400.jpg',
+    pageUrl: '/personaje/frodo-bolson',
+    theme: 'diable',
   });
 
-  bilboCharacter.save((error, result) => saveElHobbit1(error, result, laComarca.id));
-
-  return bilboCharacter;
+  doc.save((error, result) => save3(error, result));
 }
 
-saveElHobbit1 = function (error, bilbo, laComarcaId) {
+save3 = function (error, result) {
   if (error) return console.error(error);
-  var elHobbitFilm = new Film({
-    name: 'El Hobbit: un viaje insperado',
-    releaseYear: 2012,
-    duration: 169,
-    storyTimeOrder: 1,
-    characters: bilbo.id,
-    places: [laComarcaId],
-    imageUrl: 'el-hobbit-un-viaje-inesperado',
-    pageUrl: '/pelicula/el-hobbit-1',
+  var doc = new Character({
+    name: 'Boromir',
+    demographicData: null,
+    placeOfBirth: null,
+    alive: true,
+    imageUrl: 'https://vignette.wikia.nocookie.net/lotr/images/b/b4/Seanbean_boromir.jpg/revision/latest?cb=20110327195115',
+    pageUrl: '/personaje/boromir',
+    theme: 'forest',
+  });
+
+  doc.save((error, result) => save4(error, result));
+}
+
+save4 = function (error, result) {
+  if (error) return console.error(error);
+  var doc = new Character({
+    name: 'Gandalf',
+    demographicData: null,
+    placeOfBirth: null,
+    alive: true,
+    imageUrl: 'https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-the-lord-of-the-rings-ian-mckellen.jpg',
+    pageUrl: '/personaje/gandalf',
+    theme: 'kino',
+  });
+
+  doc.save((error, result) => save5(error, result));
+}
+
+save5 = function (error, result) {
+  if (error) return console.error(error);
+  var doc = new Character({
+    name: 'Saruman',
+    demographicData: null,
+    placeOfBirth: null,
+    alive: true,
+    imageUrl: 'https://vignette.wikia.nocookie.net/lotr/images/0/0c/Christopher_Lee_as_Saruman.jpg/revision/latest?cb=20170127113833',
+    pageUrl: '/personaje/saruman',
+    theme: 'lime-sports',
+  });
+
+  doc.save((error, result) => save6(error, result));
+}
+
+save6 = function (error, result) {
+  if (error) return console.error(error);
+  var doc = new Character({
+    name: 'Galadriel',
+    demographicData: null,
+    placeOfBirth: null,
+    alive: true,
+    imageUrl: 'https://vignette.wikia.nocookie.net/lotr/images/0/0c/Christopher_Lee_as_Saruman.jpg/revision/latest?cb=20170127113833',
+    pageUrl: '/personaje/galadriel',
+    theme: 'lime-sports',
+  });
+
+  doc.save((error, result) => save7(error, result));
+}
+
+
+save7 = function (error, bilbo) {
+  if (error) return console.error(error);
+  var doc = new Film({
+    name: 'La Comunidad del Anillo',
+    releaseYear: 2001,
+    duration: 178,
+    storyTimeOrder: 4,
+    characters: [bilbo.id],
+    places: null,
+    imageUrl: 'http://elanillounico.com/wp-content/uploads/2016/12/ESDLA.-LCDA.jpg',
+    pageUrl: '/pelicula/la-comunidad-del-anillo',
+    theme: 'lollipop',
   });
 
   //When last document is saved, close connection
-  elHobbitFilm.save(function (error) {
+  doc.save(function (error) {
    if (error) return console.error(error);
+   console.log('All New Data inserted on DB, closing connection...');
    mongoose.connection.close();
  });
 }
