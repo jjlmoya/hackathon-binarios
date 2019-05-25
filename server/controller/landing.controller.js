d1f2fcc79bc73e4c8bb50384ce5552c8fbe5e581
W01fw00d <gabrm_91@hotmail.com>
2019-05-25 18:59:35 +0200

    HAIKU#11
    Crear controllers para llamar a Base de Datos
    
    Me han llevado demasiado rato

--- server/controller/landing.controller.js
+++ server/controller/landing.controller.js
@@ -1,6 +1,11 @@
 const router = require('../config').router();
 const Commons = require('../services/commons.services');
 
+const mongoose = require('mongoose');
+const ConfigDBModels = require('../config-db-models');
+
+let models = ConfigDBModels.createSchemas(mongoose);;
+
 
 module.exports = function (app) {
     router.forEach(function (corePage) {
@@ -10,10 +15,63 @@ module.exports = function (app) {
         if (corePage.pages) {
             corePage.pages.forEach(function (page) {
                 app.get(corePage.path + page.path, function (req, res) {
-                    //BBDD ->
                     res.render(page.view, Commons.join(corePage));
                 });
             });
         }
+
+        app.get('hackaton/personajes', function (req, res) {
+          //getModeloPersonajes
+            res.render(corePage.view, Commons.join(corePage));
+        });
+
+        app.get('hackaton/personaje/{character}', function (req, res) {
+          //getModelPersonaje(character)
+            res.render(corePage.view, Commons.join(corePage));
+        });
     });
 };
+
+getCharacters = function () {
+    var query = models.Character.find();
+
+    query.exec(function (error, result) {
+     if (error) return console.error(error);
+     return result;
+   });
+}
+
+getCharacter = function (characterUrl) {
+    var query = models.Character.findOne({
+      'pageUrl': characterUrl,
+    });
+
+    query.exec(function (error, result) {
+     if (error) return console.error(error);
+     return result;
+   });
+}
+
+getFilms = function () {
+    var query = models.Film
+    .find()
+    .populate('characters');
+
+    query.exec(function (error, result) {
+     if (error) return console.error(error);
+     return result;
+   });
+}
+
+getFilm = function (filmUrl) {
+    var query = models.Film
+    .findOne({
+      'pageUrl': filmUrl,
+    })
+    .populate('characters');
+
+    query.exec(function (error, result) {
+     if (error) return console.error(error);
+     return result;
+   });
+}
